3d1c750f18daac091153679e8c9ef0744e19ba51
aras <aras.strong8@gmail.com>
2021-02-21 22:14:21 -0300

    comentarios

--- src/Passport-setup.js
+++ src/Passport-setup.js
@@ -1,18 +1,35 @@
 const passport = require('passport')
 
+//o passport trabalha com +500 estrategias aqui especifica-se 
+//qual estrategia vai usar
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+//uma vez autenticado o passport vai salvar um cookie no front-end
+//+e uma sessão no back-and q pd ser em algum bd ou na memoria do servidor
+//este exemplo estamos salvando no navegador do usuario sem pedir permisão 
+//salvar dados do usuarios nos cookies do navegador pode implicar em problemas de segurança
+//em aplicação profissional peça sempre a autorização ou salve esses dados em algum LocalStoragy
 
 passport.serializeUser(function(user, done) {
     done(null, user);
   });
-  
+  //aki o passport tem q deserializar ler o cookie e transformar em bytes novamente
+  //igual veio na request nesta etapa pode haver erro então é bom trate com try/catch
   passport.deserializeUser(function(user, done) {
       done(null, user);
   });
 
+//aqui instancia o construtor de estrategia google-oauth20
+//indentificando qual api ele ira usar para fazer a autenticação
+//a chave, o segredo e a url de callback são fornecidas na api do google
+//em estrategias locais vc especifica quais são os campos que vai vir do login
+//que ele usa para fazer a autenticação e dps a autenticação em si
 passport.use(new GoogleStrategy({
+  //chave
     clientID: "198105665708-0qukg43q0r1iifd093clpmlt9abcnrs5.apps.googleusercontent.com",
+  //segredo
     clientSecret: "_7V6XlNTF24BuCdAB3O-QZF8",
+  //callback
     callbackURL: "http://localhost:3000/google/callback" //pois vai rodar em localhost
   },
   
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const passport = require('passport')
 const cookieSession = require('cookie-session')
 
+//CHAMA O MODULO DE AUTENTICAÇÃO
 require('./Passport-setup')
 
 app.use(cors())
@@ -12,12 +13,18 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
 
-//SALVA OS DADOS NO COOKIE DO NAVEGADOR
+//SALVA OS DADOS NO COOKIE DO NAVEGADOR 
+//chame antes das chamadas para as rotas para garantir que não chame nenhuma rota antes da autenticação
 app.use(cookieSession({
   name: 'exemplo-session',
-  keys: ['key1', 'key2']
+  keys: ['key1', 'key2'],
+  //determina o tempo em que a session expira
+  maxAge: 2 * 60 * 1000
 }))
 
+//verifica se no header da requisição existe um objeto user
+//se for verdade o acesso a pagina é liberado 
+//se for falso uma resposta de não autorizado é mandada
 const islogin = (req,res,next) => {
   if(req.user){
     next()
@@ -32,19 +39,19 @@ app.use(passport.session())
 app.get('/', function (req, res) {
   res.send('inicio voce não esta logado')
 })
- 
+//ROTA PROTEGIDA
 app.get('/sucesso', islogin, function (req, res) {console.log(req),res.send(`bem vindo ${req.user._json.email}`)})
 app.get('/falha', function (req, res) {res.send('falaha no login')})
 
-
-app.post('/google', passport.authenticate('google',{scope:['profile','email']}))
+//CHAMADA A API
+app.get('/google', passport.authenticate('google',{scope:['profile','email']}))
 app.get('/google/callback', passport.authenticate('google',{failureRedirect: '/falha'}),
 function(req,res) {
   res.redirect('/sucesso')
 }
 
 )
-
+//ROTA DE LOGOUT INVALIDA A SESSION E REDIRECIONA PARA O INICIO /
 app.get('/logout', (req,res)=>{
   req.session=null; 
   req.logOut(); 
